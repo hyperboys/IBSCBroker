e7d19d5506537a02ee00e5555b8ef6a69cda1c9c
hyperboys <isaraboonpasert@gmail.com>
2017-01-20 00:39:00 +0700

    Add Project

---
+++ IBSC.WindowApp/IBSC.WindowApp/FocusAdvancement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace IBSC.WindowApp
+{
+    public static class FocusAdvancement
+    {
+        public static void KeyUp(object sender, KeyEventArgs e)
+        {
+            if (!e.Key.Equals(Key.Enter) || !e.Key.Equals(Key.Return)) return;
+
+            var element = sender as UIElement;
+            if (element != null) element.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+        }
+    }
+}
---
+++ IBSC.WindowApp/IBSC.WindowApp/Login.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IBSC.WindowApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignOn_Click(object sender, RoutedEventArgs e)
+        {
+            Alert.Visibility = Visibility.Visible;
+            MainWindow m = new MainWindow();
+            m.Show();
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtUser.Focus();
+            Alert.Visibility = Visibility.Hidden;
+        }
+
+        private void txtUser_KeyUp(object sender, KeyEventArgs e)
+        {
+            FocusAdvancement.KeyUp(sender,e);
+        }
+
+        private void txtPass_KeyUp(object sender, KeyEventArgs e)
+        {
+            FocusAdvancement.KeyUp(sender, e);
+        }
+    }
+}
